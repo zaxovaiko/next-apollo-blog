5c8f97096b9a4c9a1186f0e3c23caeed75f7f074
Volodymyr Zakhovaiko <zaxovaiko@gmail.com>
2022-10-22 21:53:24 +0200

    Changed workflow a bit

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'http://localhost:3000/api/graphql',
+  schema: './typeDefs.ts',
   generates: {
     'generated/graphql.ts': {
       plugins: [
