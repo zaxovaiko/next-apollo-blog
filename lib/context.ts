6944e1f78988932279a7d05fd7d6f5b2e2b1e77d
Volodymyr Zakhovaiko <zaxovaiko@gmail.com>
2022-10-23 23:32:50 +0200

    Removed console.log

--- lib/context.ts
+++ lib/context.ts
@@ -1,10 +1,4 @@
-import { MicroRequest } from 'apollo-server-micro/dist/types';
-import { ServerResponse } from 'http';
-
-export const createContextHandler = (
-  req: MicroRequest,
-  res: ServerResponse,
-) => {
-  console.log(req, res);
+export const createContextHandler = () => {
+  // TODO: Add JWT token parsing context
   return {};
 };
