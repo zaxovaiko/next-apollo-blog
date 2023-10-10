00a7944754f224046c888e70842d9cc1c78b78dc
Volodymyr Zakhovaiko <zaxovaiko@gmail.com>
2023-10-10 19:59:25 +0200

    Migrated to apollo-server-integration-next

--- server/lib/apollo.ts
+++ server/lib/apollo.ts
@@ -1,8 +1,7 @@
+import { ApolloServer } from '@apollo/server';
 import { makeExecutableSchema } from '@graphql-tools/schema';
-import { ApolloServer } from 'apollo-server-micro';
 import { DateTimeTypeDefinition, DateTimeResolver } from 'graphql-scalars';
 
-import { createContextHandler } from './context';
 import { NodeEnvs } from './enums';
 import GraphQLSchema from '../../schema';
 import { prisma } from '../prisma';
@@ -21,7 +20,6 @@ export const createServer = () =>
     schema,
     cache: 'bounded',
     introspection: process.env.NODE_ENV === NodeEnvs.Development,
-    context: createContextHandler,
   });
 
 export const startServer = async (server: ApolloServer) => {
